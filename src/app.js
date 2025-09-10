91c57facdc41beba84963e74f5e247960a8f49a6
Zulu <juanpablozuluaga.0311@gmail.com>
2025-09-10 09:50:21 -0500

    Política de CORS

--- src/app.js
+++ src/app.js
@@ -6,7 +6,10 @@ import tarjetaRouter from "./routers/tarjetaRouter.js"
 const app = express();
 
 app.use(express.json());
-app.use(cors())
+app.use(cors({
+      origin: "https://www.devcorebits.com"
+}
+))
 app.use(express.urlencoded({ extended: true }));
 
 
